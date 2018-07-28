4af9cdf3c54d4d55b80f64c81fe5e0d9cd4eef5b
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-07-27 21:11:40 -0500

    Add project files.

---
+++ CG 3-1 Months/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_3_1_Months
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
